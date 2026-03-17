3bb514fff23d4007089918731b6ac226a4819bbe
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-16 22:41:12 -0500

    Updated CabinClass.java

--- ai-airline-application/src/main/java/com/airline/ai/entity/enums/CabinClass.java
+++ ai-airline-application/src/main/java/com/airline/ai/entity/enums/CabinClass.java
@@ -1,4 +1,20 @@
 package com.airline.ai.entity.enums;
 
+import lombok.Getter;
+
+@Getter
 public class CabinClass {
+
+    ECONOMY        ("Y", 1.0),
+    PREMIUM_ECONOMY("W", 1.8),
+    BUSINESS       ("C", 3.5),
+    FIRST          ("F", 6.0);
+
+    private final String code;
+    private final double priceMultiplier;
+
+    CabinClass(String code, double priceMultiplier) {
+        this.code            = code;
+        this.priceMultiplier = priceMultiplier;
+    }
 }
