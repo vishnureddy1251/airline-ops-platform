6d20fa48211cb6e13e7d26ac430e4b91599176de
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-16 22:21:13 -0500

    Updated BaseEntity.java

--- ai-airline-application/src/main/java/com/airline/ai/entity/BaseEntity.java
+++ ai-airline-application/src/main/java/com/airline/ai/entity/BaseEntity.java
@@ -6,8 +6,24 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Getter @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
+
+
+    private UUID id;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private String createdBy;
+
+    private String updatedBy;
+
+    private Long version;
 }
