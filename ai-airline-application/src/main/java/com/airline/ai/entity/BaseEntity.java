3f1171bb2d047a67e7a87e46afa57c1519d5636c
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-16 22:24:05 -0500

    Updated BaseEntity.java

--- ai-airline-application/src/main/java/com/airline/ai/entity/BaseEntity.java
+++ ai-airline-application/src/main/java/com/airline/ai/entity/BaseEntity.java
@@ -1,9 +1,12 @@
 package com.airline.ai.entity;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -14,16 +17,28 @@ import java.util.UUID;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
     private UUID id;
 
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
     private String createdBy;
 
+    @LastModifiedBy
+    @Column(name = "updated_by")
     private String updatedBy;
 
+    @Version
+    @Column(name = "version")
     private Long version;
 }
