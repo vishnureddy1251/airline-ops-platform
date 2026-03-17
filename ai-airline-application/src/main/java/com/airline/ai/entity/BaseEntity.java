db220d2969a491e6ef8247cecb9d81abf02696f2
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-16 22:07:49 -0500

    Added BaseEntity.java

---
+++ ai-airline-application/src/main/java/com/airline/ai/entity/BaseEntity.java
@@ -0,0 +1,13 @@
+package com.airline.ai.entity;
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter @Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+}
