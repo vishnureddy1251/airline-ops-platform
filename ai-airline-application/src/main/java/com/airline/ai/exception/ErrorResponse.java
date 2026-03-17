d8bff193739f72c84c5f5e41a9256e5be13ad497
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-16 22:52:29 -0500

    Added ErrorResponse.java

---
+++ ai-airline-application/src/main/java/com/airline/ai/exception/ErrorResponse.java
@@ -0,0 +1,10 @@
+package com.airline.ai.exception;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+@Data @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class ErrorResponse {
+}
