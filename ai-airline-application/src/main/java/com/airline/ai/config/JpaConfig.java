34e4b954852ad906ff6b6c7ec2131a597ef01375
vishnureddy1251 <80110763+vishnureddy1251@users.noreply.github.com>
2026-03-17 21:28:46 -0500

    Added JpaConfig.java

---
+++ ai-airline-application/src/main/java/com/airline/ai/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.airline.ai.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
+public class JpaConfig {
+}
